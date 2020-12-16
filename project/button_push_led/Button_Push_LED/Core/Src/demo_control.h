0447d9552e3155749379d619c918ef3ecdab4c00
akklaw <akklaw@hotmail.com>
2020-12-15 18:25:34 -0800

    Project: Button Push LEDs

---
+++ project/button_push_led/Button_Push_LED/Core/Src/demo_control.h
@@ -0,0 +1,3 @@
+// control which set of code to run
+//#define DEMO_CODE  // code was written in the class
+//#undef DEMO_CODE   // using my code
---
+++ project/button_push_led/Button_Push_LED/Core/Src/led_states.h
@@ -0,0 +1,13 @@
+#ifndef __LED_STATES_H_
+#define __LED_STATES_H_
+
+enum LED_STATE {
+  LED_OFF,
+  LED_SOLID,
+  LED_BLINK_SLOW,
+  LED_BLINK_FAST,
+  LED_DIM_SLOW,
+  LED_DIM_FAST,
+  LED_STATE_LAST
+};
+#endif
